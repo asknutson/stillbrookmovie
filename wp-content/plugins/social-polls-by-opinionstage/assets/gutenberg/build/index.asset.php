3a1a9f7f6c6d2679f1443ffce9fbe98031fdebee
Andrew <andrew.knutson@mktg.com>
2021-09-20 12:14:55 -0400

    wp fresh

--- wp-content/plugins/social-polls-by-opinionstage/assets/gutenberg/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '291995cae46d6697f72712de8f6e9022');
--- wp-content/themes/xpand-blog/template-parts/content-page.php
+++
@@ -1,5 +0,0 @@
-<?php
-if(has_post_thumbnail()){
-the_post_thumbnail(); 
-} ?>                      
-<?php the_content();wp_link_pages(); ?>        
